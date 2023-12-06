9f14dfc5815e2c0ff85fc2a546f73fb30f623a87
养老鱼 <1611131463@qq.com>
2023-12-06 16:31:19 +0800

    fix

--- src/test/java/org/example/L2021113647_18_Test.java
+++ src/test/java/org/example/L2021113647_18_Test.java
@@ -8,8 +8,8 @@ public class L2021113647_18_Test {
     //全部正数
     @Test
     public void test1(){
-        int input[]={1,2,3,4};
-        int output[];
+        int[] input={1,2,3,4};
+        int[] output;
         Solution solu =new Solution();
         output=solu.productExceptSelf(input);
         Assert.assertArrayEquals(new int[]{24,12,8,6},output);
@@ -18,8 +18,8 @@ public class L2021113647_18_Test {
     //全为负数
     @Test
     public void test2(){
-        int input[]={-1,-2,-3,-4};
-        int output[];
+        int[] input={-1,-2,-3,-4};
+        int[] output;
         Solution solu =new Solution();
         output=solu.productExceptSelf(input);
         Assert.assertArrayEquals(new int[]{-24,-12,-8,-6},output);
@@ -28,8 +28,8 @@ public class L2021113647_18_Test {
     //正数负数都有
     @Test
     public void test3(){
-        int input[]={-1,2,-3,4};
-        int output[];
+        int[] input={-1,2,-3,4};
+        int[] output;
         Solution solu =new Solution();
         output=solu.productExceptSelf(input);
         Assert.assertArrayEquals(new int[]{-24,12,-8,6},output);
@@ -37,8 +37,8 @@ public class L2021113647_18_Test {
     //包含0，正数
     @Test
     public void test4(){
-        int input[]={1,1,0,3,3};
-        int output[];
+        int[] input={1,1,0,3,3};
+        int[] output;
         Solution solu =new Solution();
         output=solu.productExceptSelf(input);
         Assert.assertArrayEquals(new int[]{0,0,9,0,0},output);
@@ -46,8 +46,8 @@ public class L2021113647_18_Test {
     //0和负数
     @Test
     public void test5(){
-        int input[]={-1,-1,0,-3,-3};
-        int output[];
+        int[] input={-1,-1,0,-3,-3};
+        int[] output;
         Solution solu =new Solution();
         output=solu.productExceptSelf(input);
         Assert.assertArrayEquals(new int[]{0,0,9,0,0},output);
@@ -56,8 +56,8 @@ public class L2021113647_18_Test {
     //0，正数，负数
     @Test
     public void test6(){
-        int input[]={1,-1,0,3,-3};
-        int output[];
+        int[] input={1,-1,0,3,-3};
+        int[] output;
         Solution solu =new Solution();
         output=solu.productExceptSelf(input);
         Assert.assertArrayEquals(new int[]{0,0,9,0,0},output);
